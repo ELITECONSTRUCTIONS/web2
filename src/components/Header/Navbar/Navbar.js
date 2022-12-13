8a04ff765876b4e5ef76526ae2e033dc54251966
Amrik Singh Khalsa <amriksingh.khalsa@honeywell.com>
2022-12-14 00:56:34 +0530

    blog removed

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -2,6 +2,38 @@ import React from 'react';
 import NavbarItem from './NavbarItem'
 
 function Navbar(props) {
+    /*  {
+    "id": 5,
+    "title": "Blog",
+    "link": "/blog-grid-right-sidebar",
+    "megaMenu": false,
+    "subMenu": [
+      {
+        "title": "Blog Grid Left Sidebar",
+        "link": "/blog-grid-left-sidebar"
+      },
+      {
+        "title": "Blog Grid Right Sidebar",
+        "link": "/blog-grid-right-sidebar"
+      },
+      {
+        "title": "Blog Grid Without Sidebar",
+        "link": "/blog-grid-without-sidebar"
+      },
+      {
+        "title": "Blog List Left Sidebar",
+        "link": "/blog-list-left-sidebar"
+      },
+      {
+        "title": "Blog List Right Sidebar",
+        "link": "/blog-list-right-sidebar"
+      },
+      {
+        "title": "Blog Details",
+        "link": "/blog/wild-life-workshop?id=14"
+      }
+    ]
+  }, */
     return (
         <ul className="main-menu nav">
             <NavbarItem />
