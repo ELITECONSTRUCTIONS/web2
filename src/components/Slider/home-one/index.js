568df03a380f45e11b35d7ea2452c7e0c2254f0f
Amrik Singh Khalsa <amriksingh.khalsa@honeywell.com>
2022-12-14 00:10:01 +0530

    recent changes

--- src/components/Slider/home-one/index.js
+++ src/components/Slider/home-one/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SlickSlider from '../../UI/Slick'
 import SliderData from '../../../data/Slider/home-1'
 import {Link} from "react-router-dom";
-
+import './home-one.css';
 
 const NextArrow = ({className, onClick}) => {
     return (
@@ -40,11 +40,11 @@ const Slider = () => {
                         SliderData.map(item => (
                             <div key={item.id}>
                                 <div className="slider-item"
-                                     style={{backgroundImage: `url(${require('../../../assets/img/' + item.bg)})`}}>
+                                     style={{backgroundImage: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),url(${require('../../../assets/img/' + item.bg)})`}}>
                                     <div className="container">
                                         <div className="row">
                                             <div className="col-xl-7">
-                                                <div className="slider-content">
+                                                <div className="slider-content" style={{opacity:1}}>
                                                     <h2>{item.title}</h2>
                                                     <p>{item.text}</p>
                                                     <Link to={`${process.env.PUBLIC_URL + item.btnLink}`} className="btn btn-brand">{item.btnText}</Link>
