a78bf71097f49381e1d24fa88a3fed902192744c
Amrik Singh Khalsa <amriksingh.khalsa@honeywell.com>
2023-01-18 02:32:50 +0530

    image hyperlinks area created

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -1,10 +1,10 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import PageWrapper from "../../components/PageWrapper";
 import PageHeader from "../../components/PageHeader";
 import BlogContent from "./BlogContent";
 import SidebarForBlog from "../SidebarForBlog";
-
-const BlogPage = ({sidebar_position,blog_type,sidebar}) => {
+import projectImg from "../../assets/img/planningBoards.jpg"
+const BlogPage = ({ sidebar_position, blog_type, sidebar }) => {
     return (
         <Fragment>
             <PageHeader
@@ -14,11 +14,21 @@ const BlogPage = ({sidebar_position,blog_type,sidebar}) => {
             />
 
             <PageWrapper classes={'blog-page-content-area sp-y'}>
-                {sidebar === true && sidebar_position === 'left' ? <SidebarForBlog classes={'order-1 order-lg-0'}/> : null}
-
-                <BlogContent blog_type={blog_type} cols={sidebar ? 'col-lg-9' : 'col-12'} classes={sidebar_position === 'left' ? 'order-0 order-lg-1':null}/>
-
-                {sidebar === true && sidebar_position === 'right' ? <SidebarForBlog/> : null}
+                {sidebar === true && sidebar_position === 'left' ? <SidebarForBlog classes={'order-1 order-lg-0'} /> : null}
+                {/*                  <BlogContent blog_type={blog_type} cols={sidebar ? 'col-lg-9' : 'col-12'} classes={sidebar_position === 'left' ? 'order-0 order-lg-1':null}/>
+ */}                 <img src={projectImg} usemap="#gfg_map" onClick={(e)=>{
+                        console.log("coor",e,e.screenX,e.screenY)
+ }} />
+                <map name="gfg_map" color='red'>
+                    {/**660,580,910,780 */}
+                    <area shape="rect" coords="462,15,748,200" alt="GFG1"
+                        target="_blank" href="" style={{color:"red"}} onClick={(e)=>console.log("curr",e)}/>
+                    <area shape="rect" coords="0,280, 390,740" alt="GFG2"
+                        href="https://www.geeksforgeeks.org" target="_blank" />
+                    <area shape="rect" coords="950,280, 1500,740" alt="GFG3"
+                        href="https://github.com" target="_blank" />
+                </map>
+                {sidebar === true && sidebar_position === 'right' ? <SidebarForBlog /> : null}
             </PageWrapper>
         </Fragment>
     );
